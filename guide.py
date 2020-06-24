5b5ce6c71f2230c90fe0854ce458c5e06358fa9c
Sri Lakshmi <srilakshmiimhskalirs@gmail.com>
2020-06-24 22:06:38 +0530

    Update guide.py

--- guide.py
+++ guide.py
@@ -8,15 +8,31 @@ def from_author():
 	#st.subheader('From the bottom of my heart, I would like to Thank the Intelligent Inventors & Developers of the Open Source Softwares & the supportive knowledge-sharing Open Source Community…!!!')
 
 	st.markdown('''
-		### Dear Viewer, Hello...!!!
+		### Hello...!!!
 
 		### Hope you & your family are well,safe & healthy...
 
 		### I am Sri Lakshmi, the author of CoViz.This is my first Live Data Project.
 
 		### From the bottom of my heart, I would like to Thank the Intelligent Inventors & Developers of the Open Source Softwares & the supportive knowledge-sharing Open Source Community…!!!
+		
+		## CoViz (Corona Virus Visualisation ) is a Python Web Application that provides live Visual Analysis of Coronavirus data from around the World.
 
-		### CoViz (Corona Virus Visualisation ) is a Web Application written in Python.
+		## COVIZ APP DETAILS:
+
+		### Language : PYTHON
+
+		### FrameWork : STREAMLIT
+
+		### Visualization : PLOTLY
+
+		### Data Manipulation : PANDAS
+
+		### Deployed on : HEROKU
+		
+		### Dataset : John Hopkins University Data
+
+		### COVIZ APP GUIDE :
 
 		### Tip: You can hover over any Graph, to see the details in detail.
 
@@ -39,6 +55,8 @@ def from_author():
 		### I just hope that this unfortunate situation will end in the very near future.
 
 		### STAY SAFE ... STAY HEALTHY ... BELIEVE IN YOURSELF...
+		
+		## FINAL THOUGHTS : 
 
 		### I hope you liked the CoViz App.
 
@@ -48,7 +66,7 @@ def from_author():
 
 		### I welcome opportunities to work & collabarate...
 
-		### Feel free to connect with me on [Twitter](https://twitter.com/iamsrilakshmi) 
+		### Feel free to connect with me on [Twitter](https://twitter.com/iamsrilakshmi) [Github](https://github.com/iamsrilakshmi)[Linkedin](https://www.linkedin.com/in/iamsrilakshmi) 
 
 		### Thank you for spending your precious time on this CoViz App. 
 
